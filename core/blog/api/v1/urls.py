518d3b37a5e0d709d13e2607af14b163f7b4a333
Shahriar <shahriar.rsshkn75767777@gmail.com>
2022-11-17 14:28:45 -0800

    use routers
    use DefaultRouter for urls setting

--- core/blog/api/v1/urls.py
+++ core/blog/api/v1/urls.py
@@ -1,12 +1,21 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
 from . import views
 
 app_name = "api-v1"
 
-urlpatterns = [
-    path('post/',views.PostList.as_view(), name="post-list"),
-    path('post/<int:id>',views.PostDetail.as_view(), name="post-list")
-    
-    
-]
+router = DefaultRouter()
+router.register('post',views.PostViewSet,basename='post')
+urlpatterns = router.urls
+
+#urlpatterns = [
+    #path('post/<int:id>',views.PostDetail, name="post-detail"),
+    #path('post/',views.PostList.as_view(), name="post-list"),
+    #path('post/<int:id>',views.PostDetail.as_view(), name="post-detail"),
+    #path('post/<int:id>',views.PostDetail.as_view(), name="post-list"),
+    #path('post/',views.PostViewSet.as_view({'get':'list','post':'create'}), name="post-list"),
+   # path('post/<int:pk>',views.PostViewSet.as_view({'get':'retrieve','put':'update','patch':'partial_update','delete':'destroy'}), name="post-list")
+
+#]
+    #path('post/',views.PostList, name="post-list"),
--- core/blog/api/v1/views.py
+++ core/blog/api/v1/views.py
@@ -4,8 +4,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.shortcuts import get_object_or_404
-from rest_framework.generics import RetrieveUpdateDestroyAPIView
+from rest_framework.generics import ListCreateAPIView,RetrieveUpdateDestroyAPIView
 from rest_framework import mixins
+from rest_framework import viewsets
 
 from .serializers import PostSerializer
 from ...models import Post
@@ -118,6 +119,32 @@ class PostDetail(RetrieveUpdateDestroyAPIView):
 
 
     
+class PostViewSet(viewsets.ViewSet):
+    permission_classes = [IsAuthenticated]
+    serializer_class = PostSerializer
+    queryset = Post.objects.filter(status=True)
+
+    def list(self, request):
+        serializer = self.serializer_class(self.queryset)
+        return Response(serializer.data)
+
+    def create(self, request):
+        pass
+
+    def retrieve(self, request, pk=None):
+        post_object = get_object_or_404(self.queryset)
+        serializer = self.serializer_class(post_object)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        pass
+
+    def partial_update(self, request, pk=None):
+        pass
+
+    def destroy(self, request, pk=None):
+        pass
+
 
 
  
